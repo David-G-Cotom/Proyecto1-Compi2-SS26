ec3d63fbea745d4b54e422163c3679cb3bc88554
David-G-Cotom <brandoncotom202231460@cunoc.edu.gt>
2026-09-08 23:39:25 -0600

    Creacion del proyecto utilizando NetBeans y Maven.

---
+++ proyecto1_compi2_ss26/src/main/java/com/mycompany/proyecto1_compi2_ss26/Proyecto1_compi2_ss26.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1_compi2_ss26;
+
+/**
+ *
+ * @author david
+ */
+public class Proyecto1_compi2_ss26 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
